df02cffc9fc66431184bf509361235624139b4ee
johannbruneau <johannbruneau@protonmail.com>
2016-10-26 11:21:55 -0400

    Revert "Revert "ca bosse en cours""
    This reverts commit 4b0e2ba1441d6c6bd8fe2f03b576e19d277017cc.
    
     Veuillez saisir le message de validation pour vos modifications. Les lignes
     commençant par '#' seront ignorées, et un message vide abandonne la validation.
     Sur la branche master
     Votre branche est à jour avec 'origin/master'.
    
     Modifications qui seront validées :
    	modifié:         application.c
    	modifié:         charDriver.c
    
     Modifications qui ne seront pas validées :
    	supprimé:        ELE784-Lab1-A13.pdf
    	supprimé:        ELE784_-_Exemple_de_Pilote_et_lab/HelloDev.c
    	supprimé:        ELE784_-_Exemple_de_Pilote_et_lab/HelloDevv2.c
    	supprimé:        ELE784_-_Exemple_de_Pilote_et_lab/Makefile
    	supprimé:        ELE784_-_Exemple_de_Pilote_et_lab/TestDev.c
    	supprimé:        Makefile
    	supprimé:        Notes cours et lab.odt
    	supprimé:        src/.charDriver.ko.cmd
    	supprimé:        src/.charDriver.mod.o.cmd
    	supprimé:        src/.charDriver.o.cmd
    	supprimé:        src/.tmp_versions/charDriver.mod
    	supprimé:        src/Module.symvers
    	supprimé:        src/charDriver.ko
    	supprimé:        src/charDriver.mod.c
    	supprimé:        src/charDriver.mod.o
    	supprimé:        src/charDriver.o
    	supprimé:        src/modules.order

--- application.c
+++ application.c
@@ -12,70 +12,120 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
 void clrBuffer(void) {
     int c;
     while ((c=getchar()) != '\n' && c != EOF)
         ;
 }
 
+void clrTerminal(void) {
+     printf("\033[H\033[J"); //Clear terminal screen
+}
+
 int main(void) {
 
-		char userChoice = '0';
+		char modeChoice = '0';
+		char actionChoice = '0';
 
-        while (userChoice != '3'){
+        while (modeChoice != '3'){
             printf("---Pilote Char | Application de test---\n\n");
             printf("Menu Principal : \n 1) Ouvrir et utiliser le pilote char\n 2) Relacher le pilote char\n 3) Quitter \n");
-            scanf("%c", &userChoice);
+            scanf("%c", &modeChoice);
 
-            printf("\033[H\033[J"); //Clear screen
-            //Open the char device
-            if (userChoice=='1'){
+            clrTerminal();
 
-                //Ouverture du module :
-                fd = open("/dev/pts/0", O_RDWR);
-                //fd = open("/dev/pts/0", O_RDWR);
+            if (modeChoice=='1'){
 
-                userChoice='0';
-                while (userChoice!='4'){
+                while (modeChoice!='5'){
                     printf("----------------------------------\n");
                     printf("--Ouvrir et utiliser le pilote:---\n");
-                    printf("----------------------------------\n");
-                    printf("Operations possibles :\n 1) Read Buf\n 2) Write Buf\n 3)Configure Buf\n \n 4) pour retourner au menu\n");
-                    scanf("%c", &userChoice);
+                    printf("----------------------------------\n \n");
+                    printf("Operations possibles :\n 1) Read \n 2) Write\n 3)Read & Write \n 4)Configure Buffer\n \n 5) pour retourner au menu\n");
+                    scanf("%c", &modeChoice);
                     clrBuffer();
 
-                    switch (userChoice)
+                    switch (modeChoice)
                     {
                         case '1':  //Read from Buffer
+                            clrTerminal();
+                            printf("------------Read Buffer-----------\n");
+                            printf("----------------------------------\n \n");
+                            printf("    1) Block Read \n    2) Non Block Read \n)");
+                            scanf("%c", &actionChoice);
+                            clrBuffer();
 
-                            //ret = read(fd, &BufIn[n], 9-n);
+                            switch (actionChoice){
+                                case '1' :
+                                        //Ouverture device en bloquant      devFile = open("/dev/charDriverDev", O_RDWR);
+                                        //Fonction/Bloucle de lecture bloquante
+                                    break;
+                                case '2' :
+                                        //ouverture device en non bloquant
+                                        //Fonction/Boucle de lecture non bloquante
+                                    break;
+                            }
+
+                            /*
+                            if (devFile < 0){
+                                printf("Error : charDriverDev open");
+                                userChoice = 4;
+                            }
+                            printf("Module charDriverDev open");
+                            */
+
                             break;
 
                         case '2':  //Write to buffer
+                            clrTerminal();
+                            printf("------------Write Buffer-----------\n");
+                            printf("----------------------------------\n \n");
+                            printf("    1) Block write \n    2) Non Block Write \n) ");
+                            scanf("%c", &actionChoice);
+                            clrBuffer();
 
+                            switch (actionChoice){
+                                case '1' :
+                                        //Ouverture device en bloquant      devFile = open("/dev/charDriverDev", O_RDWR);
+                                        //Fonction/Bloucle d'ecriture bloquante
+                                    break;
+                                case '2' :
+                                        //ouverture device en non bloquant
+                                        //Fonction/Boucle d'ecriture non bloquante
+                                    break;
+                            }
                             //ret = write(fd, &BufOut[n], 1);
                             break;
 
                         case '3':  //Configure the module with IOCTL
                             break;
 
-                        case '4':  //return to the main menu
+                        case '4':  //Configure the module with IOCTL
+                            break;
+
+
+                        case '5':  //return to the main menu
                             printf("Return to the main menu \n \n \n");
                             break;
                     }
+
+                    modeChoice = 0; //clear user choice
+                    clrTerminal();
+
                 }
 
             }
 
             //Close the char device
-            else if(userChoice=='2'){
+            else if(modeChoice=='2'){
                 printf("L'application relache le pilote \n");
 
                 //close(fd);
                 return EXIT_SUCCESS;
             }
             //App close
-            else if(userChoice=='3'){
+            else if(modeChoice=='3'){
                 printf("Fermeture de l'application \n");
             }
             else{
